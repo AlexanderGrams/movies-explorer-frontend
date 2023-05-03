7a9a91867ab93d7489ebe679a4ce28e578f1c55f
AlexanderGrams <99349751+AlexanderGrams@users.noreply.github.com>
2023-05-03 16:39:29 +0300

    добавить чекбокс для выбора короткометражек и сделать адаптив этой кнопки

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,6 +1,17 @@
+import { useState } from "react";
 import "./searchForm.sass"
 
 function SearchForm() {
+  const [isActive, setIsActive] = useState(false)
+
+  function handlerClick() {
+    if(isActive) {
+      setIsActive(false)
+    } else {
+      setIsActive(true)
+    }
+  }
+
   return (
     <section className="search-form">
       <div className="search-form__wrap">
@@ -8,6 +19,12 @@ function SearchForm() {
           <input className="search-form__input" placeholder="Фильм" type="text" required></input>
           <button className="search-form__button" type="submit" aria-label="Кнопка найти">Найти</button>
         </form>
+        <label className="search-form__checkbox">
+          <div className={`search-form__checkbox-switcher ${isActive && 'search-form__checkbox-switcher_active'}`}>
+            <button onClick={handlerClick} className={`search-form__checkbox-button ${isActive && 'search-form__checkbox-button_active'}`} type="button" aria-label="поиск по короткометражкам"></button>
+          </div>
+          <p className="search-form__checkbox-name">Короткометражки</p>
+        </label>
       </div>
     </section>
   );
