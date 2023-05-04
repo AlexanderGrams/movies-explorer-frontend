790192c52b1de816b6bc9cf37d482f532b063b05
AlexanderGrams <99349751+AlexanderGrams@users.noreply.github.com>
2023-05-04 11:14:04 +0300

    добавить компонент фильмов

---
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -0,0 +1,19 @@
+import MoviesCard from "../MoviesCard/MoviesCard";
+import "./moviescardlist.sass"
+import images from "../../images/logo.svg"
+
+function MoviesCardList() {
+  return (
+    <section className="card-list">
+      <div className="card-list__wrap">
+        <ul className="card-list__movies">
+          <MoviesCard title='33 слова о дизайне' time='1ч 42м' img={images} alt='какое-то описание' ilLike={true}/>
+          <MoviesCard title='33 слова о дизайне' time='1ч 42м' img={images} alt='какое-то описание' ilLike={false}/>
+        </ul>
+        <button className="card-list__button">Ещё</button>
+      </div>
+    </section>
+  );
+}
+
+export default MoviesCardList;
