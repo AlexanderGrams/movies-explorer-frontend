d1f807dbec9077f713d2fedebedd57bebd9364dc
AlexanderGrams <99349751+AlexanderGrams@users.noreply.github.com>
2023-05-06 20:27:04 +0300

    добавить логику редактирования информации пользователя

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,11 +1,18 @@
 import { Link } from "react-router-dom";
 import MainBlocks from "../MainBlocks/MainBlocks";
 import "./profile.sass"
+import { useState } from "react";
 
 function Profile({loggedIn}) {
-  let name = 'Виталий'
-  const email = 'cawa.cerber@gmail.com'
-  let greeting = `Привет, ${name}!`
+  const [isActiveEditProfile, setIsActiveEditProfile] = useState(true);
+
+  function hendlerEditProfile() {
+    setIsActiveEditProfile(!isActiveEditProfile)
+  }
+
+  let name = 'Виталий';
+  const email = 'cawa.cerber@gmail.com';
+  let greeting = `Привет, ${name}!`;
 
   return (
     <MainBlocks loggedIn={loggedIn} locationProfile={true}>
@@ -16,16 +23,22 @@ function Profile({loggedIn}) {
             <fieldset className="profile__form-fieldset">
               <label class="profile__field">
                 Имя
-                <input class="profile__input" id="name-input" type="text" required value={name} disabled/>
+                <input class={isActiveEditProfile ? "profile__input profile__input_active" : "profile__input"} id="name-input" type="text" required value={name} disabled={isActiveEditProfile ? false : true}/>
               </label>
               <div className="profile__form-line"></div>
               <label class="profile__field">
                 E-mail
-                <input class="profile__input" id="email-input" type="email" required value={email} disabled/>
+                <input class={isActiveEditProfile ? "profile__input profile__input_active" : "profile__input"} id="email-input" type="email" required value={email} disabled={isActiveEditProfile ? false : true}/>
               </label>
             </fieldset>
             {/* <span class="profile__input-error">При обновлении профиля произошла ошибка.</span> */}
-            <button className="profile__button">Редактировать</button>
+            {
+              isActiveEditProfile
+              ?
+              <button className="profile__button-saved" onClick={hendlerEditProfile}>Сохранить</button>
+              :
+              <button className="profile__button" onClick={hendlerEditProfile}>Редактировать</button>
+            }
           </form>
           <Link to={"/"} className="profile__link">Выйти из аккаунта</Link>
         </div>
