8a3e852c0ed089ea765636310dd49562c4379165
AlexanderGrams <99349751+AlexanderGrams@users.noreply.github.com>
2023-05-06 20:50:28 +0300

    изменить семантику лайков

--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -7,8 +7,8 @@ function MoviesCardList({ locationSavedPage }) {
     <section className="card-list">
       <div className="card-list__wrap">
         <ul className="card-list__movies">
-          <MoviesCard title='33 слова о дизайне' time='1ч 42м' img={images} alt='какое-то описание' ilLike={true} locationSavedPage={locationSavedPage}/>
-          <MoviesCard title='33 слова о дизайне' time='1ч 42м' img={images} alt='какое-то описание' ilLike={false} locationSavedPage={locationSavedPage}/>
+          <MoviesCard title='33 слова о дизайне' time='1ч 42м' img={images} alt='какое-то описание' isLike={true} locationSavedPage={locationSavedPage}/>
+          <MoviesCard title='33 слова о дизайне' time='1ч 42м' img={images} alt='какое-то описание' isLike={false} locationSavedPage={locationSavedPage}/>
         </ul>
         {
           locationSavedPage
