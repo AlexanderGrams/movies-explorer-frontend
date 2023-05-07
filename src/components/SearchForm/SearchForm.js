a8e35c0a898368c6b186f5a26ad273c61d4cf12e
AlexanderGrams <99349751+AlexanderGrams@users.noreply.github.com>
2023-05-07 15:49:10 +0300

    проверить и исправить ошибки валидатора HTML

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -14,18 +14,18 @@ function SearchForm() {
   }
 
   return (
-    <section className="search-form">
+    <section className="search-form" aria-label="поиск фильмов">
       <div className="search-form__wrap">
         <form className="search-form__container">
           <input className="search-form__input" placeholder="Фильм" type="text" required></input>
           <button className="search-form__button" type="submit" aria-label="Кнопка найти">Найти</button>
         </form>
-        <label className="search-form__control">
-          <div onClick={handlerClick} className={isActive ? "search-form__checkbox search-form__checkbox_active" : "search-form__checkbox"} type="button" aria-label="поиск по короткометражкам">
+        <div className="search-form__control">
+          <div onClick={handlerClick} className={isActive ? "search-form__checkbox search-form__checkbox_active" : "search-form__checkbox"}>
             <div className={isActive ? "search-form__checkbox-circle search-form__checkbox-circle_active" : "search-form__checkbox-circle"}></div>
           </div>
           <p className="search-form__checkbox-name">Короткометражки</p>
-        </label>
+        </div>
       </div>
     </section>
   );
