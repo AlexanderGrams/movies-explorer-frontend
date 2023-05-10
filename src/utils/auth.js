adcbc97e6a269916cd896928f88f38a1efb87607
AlexanderGrams <99349751+AlexanderGrams@users.noreply.github.com>
2023-05-10 20:38:53 +0300

    добавить регистрацию нового пользователя

---
+++ src/utils/auth.js
@@ -0,0 +1,17 @@
+import { BASE_URL } from "./constant"
+
+const checkResponse = (res) => {
+  return res.ok ? res.json() : Promise.reject(`Ошибка: ${res.status}`)
+}
+
+export const register = (name, email, password) => {
+  return fetch(`${BASE_URL}/signup`, {
+    method: "POST",
+    headers: {
+      "Accept": "application/json",
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({name, email, password})
+  })
+    .then(checkResponse)
+}
