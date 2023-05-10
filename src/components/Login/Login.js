a01cd681bf809b5081111dd0118fafef37ef81f9
AlexanderGrams <99349751+AlexanderGrams@users.noreply.github.com>
2023-05-10 20:40:36 +0300

    исправить ошибку зарезервированного React солва for

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -7,12 +7,12 @@ function Login() {
     <LoginAndRegister title="Рады видеть!" paragraph="Ещё не зарегистрированы?" link="/signup" linkText="Регистрация">
       <LoginAndRegisterForm>
         <div className="form">
-          <label className="form__input-name" for="email">E-mail</label>
+          <label className="form__input-name" htmlFor="email">E-mail</label>
           <input id="email" className="form__input" name="email" type="email" minLength="2" required />
           <span className="form__text-error"></span>
         </div>
         <div className="form">
-          <label className="form__input-name" for="password">Пароль</label>
+          <label className="form__input-name" htmlFor="password">Пароль</label>
           <input id="password" name="password" type="password" className="form__input form__input_type_error" minLength="2" required />
           <span className="form__text-error">Что-то пошло не так...</span>
         </div>
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -15,17 +15,17 @@ function Register({ onRegister }) {
     <LoginAndRegister title="Добро пожаловать!" paragraph="Уже зарегистрированы?" link="/signin" linkText="Войти">
       <LoginAndRegisterForm isValid={isValid} name={'register'} buttonText="Зарегистрироваться" handleSubmit={handleSubmit}>
         <div className="form">
-          <label className="form__input-name" for="nameUser">Имя</label>
+          <label className="form__input-name" htmlFor="nameUser">Имя</label>
           <input id="nameUser" className={errors.nameUser ? "form__input form__input_type_error" : "form__input"} name="nameUser" type="text" minLength="2" required onChange={handleChange}/>
           <span className="form__text-error">{errors.nameUser}</span>
         </div>
         <div className="form">
-          <label className="form__input-name" for="emailUser">E-mail</label>
+          <label className="form__input-name" htmlFor="emailUser">E-mail</label>
           <input id="emailUser" className={errors.emailUser ? "form__input form__input_type_error" : "form__input"} name="emailUser" type="email" minLength="2" required onChange={handleChange}/>
           <span className="form__text-error">{errors.emailUser}</span>
         </div>
         <div className="form">
-          <label className="form__input-name" for="passwordUser">Пароль</label>
+          <label className="form__input-name" htmlFor="passwordUser">Пароль</label>
           <input id="passwordUser" name="passwordUser" type="password" className={errors.passwordUser ? "form__input form__input_type_error" : "form__input"} minLength="2" required onChange={handleChange}/>
           <span className="form__text-error">{errors.passwordUser}</span>
         </div>
