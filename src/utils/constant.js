7abcf950e897840ca5319f5186a12f9cda510641
AlexanderGrams <99349751+AlexanderGrams@users.noreply.github.com>
2023-05-31 17:48:51 +0300

    добавить патерн валидации для полей email

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,6 +1,8 @@
 export const BASE_URL = 'https://api.films.nomoredomains.monster';
 export const MOVIES_URL = 'https://api.nomoreparties.co/beatfilm-movies';
 
+export const PATTERN_EMAIL = "[A-z0-9!#$%&'*+-/=?^_`{|]{1,64}@[A-z0-9-.]{2,253}\\.[A-z]{2,63}";
+
 export const calcCardsCounter = () => {
   const counter = { init: 12, more: 3 };
 
